7204054e376e0537d371738306fe87094147d091
1jt <2570101165@qq.com>
2023-03-29 17:18:12 +0800

    添加项目文件。

---
+++ 01 HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+//int main()
+//{
+//	cout << "hello C++" << endl;
+//	system("pause");
+//	return 0;
+//}
---
+++ 02 常量.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+// 常量的定义方式
+// 1. #define 宏常量
+// 2. const修饰的变量
+
+// 1. #define 宏常量
+#define Day 7
+
+
+//int main()
+//{
+//	// 2. const修饰的变量
+//	const int mouth = 12;
+//
+//	system("pause");
+//	return 0;
+//}
