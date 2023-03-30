934b474a8ae51fdba8ffa7328677b40b71ecadac
1jt <2570101165@qq.com>
2023-03-30 15:14:45 +0800

    运算符学习

---
+++ 05 运算符.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	// 算术运算符
+	// + - * / % ++ --
+
+	// 赋值运算符
+	// = += -= *+ /= *= %=
+
+	// 比较运算符
+	// == != < > <= >=
+
+	// 逻辑运算符
+	// ! && ||
+
+	system("pause");
+	return 0;
+}
